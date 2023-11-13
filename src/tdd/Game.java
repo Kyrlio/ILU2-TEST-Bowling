16ccd7dd91e9b0e9f443f12a116e8d2f0a87c779
Kyrlio <cedricroussel2002@gmail.com>
2023-11-13 09:14:12 +0100

    test ving quilles renvoie 20

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,13 +1,15 @@
 package tdd;
 
 public class Game {
+	private int score;
 
 	public void roll(int n) {
+		score += n;
 		score();
 	}
 
 	public Integer score() {
-		return 0;
+		return score;
 	}
 
 }
